877c066ede7bcf06ea8cd1185f06a0eafdc9c506
Adrian Renner <a.renner+git@eosn.de>
2018-03-04 15:06:28 +0100

    reindent (4 Spaces)

--- contextAction.js
+++ contextAction.js
@@ -1,11 +1,11 @@
 function getword(info,tab) {
-  console.log("Linguee search with word \"" + info.selectionText + "\" was requested.");
-  chrome.tabs.create({  
-    url: "https://www.linguee.com/search?source=auto&query=" + info.selectionText,
-  });
+    console.log("Linguee search with word \"" + info.selectionText + "\" was requested.");
+    chrome.tabs.create({  
+        url: "https://www.linguee.com/search?source=auto&query=" + info.selectionText,
+    });
 }
 chrome.contextMenus.create({
-  title: "Linguee: \"%s\"", 
-  contexts:["selection"], 
-  onclick: getword,
+    title: "Linguee: \"%s\"", 
+    contexts:["selection"], 
+    onclick: getword,
 });
