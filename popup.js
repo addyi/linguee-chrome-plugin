2c85bccca5b17dd73782c70ec11c49af8b739b73
Osilos <f.marianacci@gmail.com>
2020-05-08 14:26:41 +0200

    add force source language

--- popup.js
+++ popup.js
@@ -4,23 +4,43 @@ function openTabWithGitRepoPage() {
     });
 };
 
-const langSelect = document.getElementById('target');
+var targetLangSelect;
+var sourceLangSelect;
 
-window.addEventListener('load', function load(event){
+window.onload = function () {
+    targetLangSelect = document.getElementById('target');
+    sourceLangSelect = document.getElementById('source');
+
+    chrome.storage.sync.get(["targetLang"], (items) => {
+        targetLangSelect.value = items.targetLang;
+    })
+
+    chrome.storage.sync.get(["sourceLang"], (items) => {
+        sourceLangSelect.value = items.sourceLang || "Auto";
+    })
+}
+
+window.addEventListener('load', function load(event) {
     const openRepoButton = document.getElementById('openGitRepo');
-    openRepoButton.addEventListener('click', function() { openTabWithGitRepoPage(); });
+    openRepoButton.addEventListener('click', function () { openTabWithGitRepoPage(); });
 
-    langSelect.addEventListener("change", (e) => {
+    targetLangSelect.addEventListener("change", (e) => {
         const target = e.currentTarget;
         const value = target.options[target.selectedIndex].value;
 
-        chrome.storage.sync.set({"targetLang" : value}, () => {
-            chrome.extension.getBackgroundPage().console.log("TargetLang : " + value + " was well saved" );
+        chrome.storage.sync.set({ "targetLang": value }, () => {
+            chrome.extension.getBackgroundPage().console.log("TargetLang : " + value + " was well saved");
+        })
+    })
+    sourceLangSelect.addEventListener("change", (e) => {
+        const target = e.currentTarget;
+        const value = target.options[target.selectedIndex].value;
+
+        chrome.storage.sync.set({ "sourceLang": value }, () => {
+            chrome.extension.getBackgroundPage().console.log("SourceLang : " + value + " was well saved");
         })
     })
 });
 
-chrome.storage.sync.get(["targetLang"], (items) => {
-    langSelect.value = items.targetLang;
-})
+
 
